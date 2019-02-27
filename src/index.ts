728a9cf6160251909d2cb7bb1df0653c64d51e1a
volante1013 <u.swb01140@gmail.com>
2019-02-28 07:31:17 +0900

    カレンダーに登録する関数追加、early return追加

--- src/index.ts
+++ src/index.ts
@@ -8,27 +8,20 @@ global.doPost = (e): void => {
 	const data = JSON.parse(jsonString);
 	const tweetText: string = data.text;
 	// const tweetText: string =
-	// '次回の「艦これ」稼働全サーバ群メンテナンス＆アップデートは、今週金曜日【2/8(金)】実施を予定しています。同開始時間は【11:00】、作業完了時間は【20:30】を予定しています。提督の皆さん、本件ご留意頂けますと幸いです。';
+	// 	'「艦これ」次回稼働全サーバ群メンテナンス＆アップデートを、明日【2/27(水) 11:00】より実施予定です。同作業完了は、【20:00】を予定しています。提督の皆さん、お手数をお掛けします。ご協力、どうぞよろしくお願い致します！\n#艦これ';
 	console.log(tweetText);
 
 	// ツイートからメンテナンスの日付と時間を抽出
 	const ext = new DateExtraction(tweetText);
 	const date: string = ext.extractMaintenanceDate();
-	const time: string[] = ext.extractMaintenanceTime();
+	const times: string[] = ext.extractMaintenanceTime();
+	if (date === null) {
+		console.log('date === null');
+		return;
+	}
 
 	// メンテナンスを予定として指定したカレンダーに登録
 	const calendarID: string = PropertiesService.getScriptProperties().getProperty('CALENDAR_ID');
 	const cm: CalendarManager = new CalendarManager(calendarID);
+	cm.registerMaintenanceEvent(date, times);
 };
-
-// global.monitorKanColleTweet = (): void => {
-//   // prettier-ignore
-//   const consumerKey: string = PropertiesService.getScriptProperties().getProperty('TW_CONSUMER_KEY');
-//   // prettier-ignore
-//   const consumerSecret: string = PropertiesService.getScriptProperties().getProperty('TW_CONSUMER_SECRET');
-//   const tm = new TweetMonitor(consumerKey, consumerSecret);
-
-//   // prettier-ignore
-//   const newestTweetId: string = PropertiesService.getScriptProperties().getProperty('TW_NEWEST_TWEETID');
-//   const tweets = tm.getUserTimeline('KanColle_STAFF', newestTweetId);
-// };
